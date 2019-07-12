236ab8e01a5f848c181711357934ff61795899ff
Dat Le <lmd.dat@gmail.com>
2019-07-12 22:51:35 +0700

    [fix] builder

--- application/modules/catcool/language/vn/builder_lang.php
+++ application/modules/catcool/language/vn/builder_lang.php
@@ -5,3 +5,7 @@ $lang['module_subheading'] = 'Tạo module';
 $lang['module_name'] = 'Tên module';
 $lang['controller_name'] = 'Tên controller';
 $lang['model_name'] = 'Tên model';
+
+$lang['file_created'] = 'File đã tồn tại: %s';
+$lang['folder_created'] = 'Folder đã tồn tại: %s';
+$lang['created_success'] = 'Đã tạo module thành công';

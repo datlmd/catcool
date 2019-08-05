4af1bd0bb65920dc2bab4c3d6bc383df402550a7
Dat Le <z.dat.leminh@gree.net>
2019-08-05 09:22:26 +0700

    [fix] config

--- application/config/catcool.php
+++ application/config/catcool.php
@@ -1,8 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-$config['base_url'] = 'http://localhost/2018/catcool/';
+//$config['base_url'] = '';
 
 // language
 $config['language'] = 'vn';

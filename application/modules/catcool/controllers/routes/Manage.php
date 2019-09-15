2a3b43642f15645d28b4e53f249a93767b89b5c1
Dat Le <lmd.dat@gmail.com>
2019-09-15 21:28:41 +0700

    [fix] routes

--- application/modules/catcool/controllers/routes/Manage.php
+++ application/modules/catcool/controllers/routes/Manage.php
@@ -92,7 +92,7 @@ class Manage extends Admin_Controller
     public function index()
     {
         //phai full quyen hoac chi duoc doc
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_read'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -144,7 +144,7 @@ class Manage extends Admin_Controller
     public function create_table()
     {
         //phai full quyen
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_execute'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -169,7 +169,7 @@ class Manage extends Admin_Controller
     public function write()
     {
         //phai full quyen
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_execute'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -201,7 +201,7 @@ class Manage extends Admin_Controller
     public function add()
     {
         //phai full quyen hoac duoc them moi
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_add'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -218,10 +218,10 @@ class Manage extends Admin_Controller
             $additional_data['module']    = $this->input->post('module', true);
             $additional_data['resource']  = $this->input->post('resource', true);
             $additional_data['route']     = $this->input->post('route', true);
-            $additional_data['user_id']   = $this->ion_auth->get_user_id();
+            $additional_data['user_id']   = $this->get_user_id();
             $additional_data['published'] = (isset($_POST['published'])) ? STATUS_ON : STATUS_OFF;
 
-            if ($this->Manager->create($additional_data)) {
+            if ($this->Manager->insert($additional_data) !== FALSE) {
                 set_alert(lang('add_success'), ALERT_SUCCESS);
                 redirect(self::MANAGE_URL, 'refresh');
             } else {
@@ -246,7 +246,7 @@ class Manage extends Admin_Controller
     public function edit($id = null)
     {
         //phai full quyen hoac duoc cap nhat
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_edit'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -258,7 +258,7 @@ class Manage extends Admin_Controller
             redirect(self::MANAGE_URL, 'refresh');
         }
 
-        $item_edit = $this->Manager->get_by_id($id);
+        $item_edit = $this->Manager->get($id);
         if (empty($item_edit)) {
             set_alert(lang('error_empty'), ALERT_ERROR);
             redirect(self::MANAGE_URL, 'refresh');
@@ -270,12 +270,6 @@ class Manage extends Admin_Controller
         $this->form_validation->set_rules($this->config_form);
 
         if (isset($_POST) && !empty($_POST)) {
-            // do we have a valid request?
-//            if (valid_token() === FALSE || $id != $this->input->post('id')) {
-//                set_alert(lang('error_token'), ALERT_ERROR);
-//                redirect(self::MANAGE_URL, 'refresh');
-//            }
-
             if ($this->form_validation->run() === TRUE) {
 
                 $additional_data = $item_edit;
@@ -283,10 +277,10 @@ class Manage extends Admin_Controller
                 $additional_data['module']    = $this->input->post('module', true);
                 $additional_data['resource']  = $this->input->post('resource', true);
                 $additional_data['route']     = $this->input->post('route', true);
-                $additional_data['user_id']   = $this->ion_auth->get_user_id();
+                $additional_data['user_id']   = $this->get_user_id();
                 $additional_data['published'] = (isset($_POST['published'])) ? STATUS_ON : STATUS_OFF;
 
-                if ($this->Manager->create($additional_data, $id)) {
+                if ($this->Manager->update($additional_data, $id) !== FALSE) {
                     set_alert(lang('edit_success'), ALERT_SUCCESS);
                 } else {
                     set_alert(lang('error'), ALERT_ERROR);
@@ -315,7 +309,7 @@ class Manage extends Admin_Controller
     public function delete($id = null)
     {
         //phai full quyen hoac duowc xoa
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
+        if (!$this->acl->check_acl()) {
             set_alert(lang('error_permission_delete'), ALERT_ERROR);
             redirect('permissions/not_allowed', 'refresh');
         }
@@ -326,14 +320,11 @@ class Manage extends Admin_Controller
 
         //delete
         if (isset($_POST['is_delete']) && isset($_POST['ids']) && !empty($_POST['ids'])) {
-            if (valid_token() == FALSE) {
-                set_alert(lang('error_token'), ALERT_ERROR);
-                redirect(self::MANAGE_URL, 'refresh');
-            }
 
-            $ids         = explode(",", $this->input->post('ids', true));
-            $list_delete = $this->Manager->get_list_by_ids($ids);
+            $ids = $this->input->post('ids', true);
+            $ids = (is_array($ids)) ? $ids : explode(",", $ids);
 
+            $list_delete = $this->Manager->where('id', $ids)->get_all();
             if (empty($list_delete)) {
                 set_alert(lang('error_empty'), ALERT_ERROR);
                 redirect(self::MANAGE_URL, 'refresh');
@@ -364,7 +355,9 @@ class Manage extends Admin_Controller
             redirect(self::MANAGE_URL, 'refresh');
         }
 
-        $list_delete = $this->Manager->get_list_by_ids($delete_ids);
+        $delete_ids  = is_array($delete_ids) ? $delete_ids : explode(',', $delete_ids);
+        $list_delete = $this->Manager->where('id', $delete_ids)->get_all();
+
         if (empty($list_delete)) {
             set_alert(lang('error_empty'), ALERT_ERROR);
             redirect(self::MANAGE_URL, 'refresh');
@@ -375,43 +368,5 @@ class Manage extends Admin_Controller
         $this->data['ids']         = $delete_ids;
 
         $this->theme->load('routes/manage/delete', $this->data);
-    }
-
-    public function api_publish()
-    {
-        header('content-type: application/json; charset=utf8');
-
-        //phai full quyen hoac duoc cap nhat
-        if (!$this->acl->check_acl($this->ion_auth->get_user_id(), $this->ion_auth->is_super_admin())) {
-            echo json_encode(['status' => 'ng', 'msg' => lang('error_permission_edit')]);
-            return;
-        }
-
-        $data = [];
-        if (!$this->input->is_ajax_request()) {
-            show_404();
-        }
-
-        if (empty($_POST)) {
-            echo json_encode(['status' => 'ng', 'msg' => lang('error_json')]);
-            return;
-        }
-
-        $id        = $this->input->post('id');
-        $item_edit = $this->Manager->get_by_id($id);
-        if (empty($item_edit)) {
-            echo json_encode(['status' => 'ng', 'msg' => lang('error_empty')]);
-            return;
-        }
-
-        $item_edit['published'] = (isset($_POST['published']) && $_POST['published'] == 'true') ? STATUS_ON : STATUS_OFF;
-        if (!$this->Manager->create($item_edit, $id)) {
-            $data = ['status' => 'ng', 'msg' => lang('error_json')];
-        } else {
-            $data = ['status' => 'ok', 'msg' => lang('modify_publish_success')];
-        }
-
-        echo json_encode($data);
-        return;
     }
 }

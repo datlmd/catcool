fac09a6000292beb22f2a6cb97abcd634a858ce1
Dat Le <z.dat.leminh@gree.net>
2019-09-18 11:12:38 +0700

    [add] module google

---
+++ application/modules/google/controllers/Google.php
@@ -0,0 +1,46 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Google extends Admin_Controller
+{
+    public $config_form = [];
+    public $data        = [];
+
+    CONST MANAGE_NAME       = 'dummy';
+    public function __construct()
+    {
+        parent::__construct();
+
+        //set theme
+        $this->theme->theme(config_item('theme_admin'))
+            ->add_partial('header')
+            ->add_partial('footer')
+            ->add_partial('sidebar');
+
+        $this->theme->title(config_item('site_name'))
+            ->description(config_item('site_description'))
+            ->keywords(config_item('site_keywords'));
+
+        $this->lang->load('google', $this->_site_lang);
+
+
+
+
+    }
+
+    public function index()
+    {
+        //phai full quyen hoac chi duoc doc
+        if (!$this->acl->check_acl()) {
+            set_alert(lang('error_permission_read'), ALERT_ERROR);
+            redirect('permissions/not_allowed', 'refresh');
+        }
+
+
+        $this->data          = [];
+        $this->data['title'] = lang('list_heading');
+
+
+
+        $this->theme->load('index', $this->data);
+    }
+}

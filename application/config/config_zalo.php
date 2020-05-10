99bca3d88b421b53f0e480b4decbdec2c84be9bc
Dat Le <lmd.dat@gmail.com>
2020-05-10 14:56:32 +0700

    [add] sdk zalo & facebook

---
+++ application/config/config_zalo.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['zalo_app_id']                = '2782228093246916703';
+$config['zalo_app_secret']            = 'UPbqKJLCO2Pw5T2R38MB';
+$config['zalo_login_redirect_url']    = 'https://localhost/dev/catcool/members/manage/login/';
+$config['zalo_logout_redirect_url']   = 'members/manage/logout_zalo';
+$config['zalo_login_type']            = 'web';
+$config['zalo_auth_on_load']          = TRUE;
---
+++ application/libraries/Zalo_lib.php
@@ -0,0 +1,87 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Include the autoloader provided in the SDK
+require_once APPPATH .'third_party/Zalo/autoload.php';
+
+use Zalo\Zalo;
+use Zalo\ZaloEndPoint;
+
+Class Zalo_lib
+{
+
+    private $zalo;
+
+    private $helper;
+
+    public function __construct()
+    {
+        // Load fb config
+        $this->load->config('config_zalo');
+
+        if (!isset($this->zalo)){
+            $this->zalo = new Zalo([
+                'app_id'     => $this->config->item('zalo_app_id'),
+                'app_secret' => $this->config->item('zalo_app_secret'),
+                'callback_url' => $this->config->item('zalo_login_redirect_url'),
+            ]);
+        }
+
+        $this->helper =$this->zalo->getRedirectLoginHelper();
+
+        if ($this->config->item('zalo_auth_on_load') === TRUE){
+            // Try and authenticate the user right away (get valid access token)
+            $this->is_authenticated();
+        }
+    }
+
+    public function object(){
+        return $this->zalo;
+    }
+
+    public function is_authenticated(){
+
+        $callBackUrl = $this->config->item('zalo_login_redirect_url');
+        $oauthCode = isset($_GET['code']) ? $_GET['code'] : "THIS NOT CALLBACK PAGE !!!"; // get oauthoauth code from url params
+        $accessToken = $this->helper->getAccessToken($callBackUrl); // get access token
+        if ($accessToken != null) {
+            $expires = $accessToken->getExpiresAt(); // get expires time
+            return $accessToken;
+        }
+
+        return false;
+    }
+
+    public function get_user($access_token = null, $params = ['fields' => 'id,name,birthday,gender,phone,picture']){
+        try{
+            $response = $this->zalo->get(ZaloEndpoint::API_GRAPH_ME, $access_token, $params);
+            return $response->getDecodedBody();
+        }catch(\Zalo\Exceptions\ZaloResponseException $e){
+            return $this->logError($e->getCode(), $e->getMessage());
+        }catch (\Zalo\Exceptions\ZaloSDKException $e){
+            return $this->logError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function login_url(){
+        // Login type must be web, else return empty string
+        if($this->config->item('zalo_login_type') != 'web'){
+            return '';
+        }
+
+        // Get login url
+        return $this->helper->getLoginUrl($this->config->item('zalo_login_redirect_url'));
+    }
+
+    public function destroy_session(){
+        $this->session->unset_userdata('fb_access_token');
+    }
+
+    private function logError($code, $message){
+        log_message('error', '[ZALO PHP SDK] code: ' . $code.' | message: '.$message);
+        return ['error' => $code, 'message' => $message];
+    }
+
+    public function __get($var){
+        return get_instance()->$var;
+    }
+}

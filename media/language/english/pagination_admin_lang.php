910483df27363bc88e0ab2e3d1bbe3efebdb2bb4
Le Minh Dat <z.dat.leminh@gree.net>
2020-09-30 14:09:59 +0700

    [delete] paggination_admin & vn, english

--- media/language/english/pagination_admin_lang.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang = [
-    'pagination_first' => 'First',
-    'pagination_last'  => 'Last',
-    'pagination_next'  => '»',//Next Page
-    'pagination_prev'  => '«',//Prev Page
-];
--- media/language/vn/pagination_admin_lang.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang = [
-    'pagination_first' => 'Đầu', //First Page
-    'pagination_last'  => 'Cuối', //'Last Page'
-    'pagination_next'  => '»',//Next Page
-    'pagination_prev'  => '«',//Prev Page
-];

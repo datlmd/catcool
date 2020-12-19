26033a92769c53ebeff263433477f3eb9aa71499
datlmd <lmd.dat@gmail.com>
2020-12-19 16:09:03 +0700

    [add] zones

---
+++ media/language/english/countries_zones_manage_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang["heading_title"] = "Zones";
+$lang["text_add"] = "Add Zone";
+$lang["text_code"] = "Code";
+$lang["text_country"] = "Country";
+$lang["text_edit"] = "Edit Zone";
+$lang["text_list"] = "Zone List";
---
+++ media/language/vn/countries_zones_manage_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang["heading_title"] = "Tỉnh Thành";
+$lang["text_add"] = "Thêm Tỉnh / Thành";
+$lang["text_code"] = "Code";
+$lang["text_country"] = "Quốc Gia";
+$lang["text_edit"] = "Cập nhật Tỉnh / Thành";
+$lang["text_list"] = "Danh sách Tỉnh / Thành";
